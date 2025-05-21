92b3b9f02a88de048b2888939c727d759e5a4579
slonka <slonka@users.noreply.github.com>
2025-05-21 11:16:30 +0200

    feat(generator): generation works
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- internal/provider/api_resource.go
+++ internal/provider/api_resource.go
@@ -146,7 +146,7 @@ func (r *APIResource) Schema(ctx context.Context, req resource.SchemaRequest, re
 				Computed:    true,
 				Optional:    true,
 				ElementType: types.StringType,
-				MarkdownDescription: `Labels store metadata of an entity that can be used for filtering an entity list or for searching across entity types. ` + "\n" +
+				MarkdownDescription: `Labels store metadata of an entity that can be used for filtering an entity list or for searching across entity types.` + "\n" +
 					`` + "\n" +
 					`Keys must be of length 1-63 characters, and cannot start with "kong", "konnect", "mesh", "kic", or "_".`,
 			},
