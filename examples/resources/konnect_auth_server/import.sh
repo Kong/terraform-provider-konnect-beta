417ee6d01b3a82f3dcbee96eeeddb4452872ade9
Michael Heap <m@michaelheap.com>
2025-06-05 12:30:33 +0100

    Regenerate provider

---
+++ examples/resources/konnect_auth_server/import.sh
@@ -0,0 +1,1 @@
+terraform import konnect_auth_server.my_konnect_auth_server ""
---
+++ examples/resources/konnect_auth_server_scopes/resource.tf
@@ -0,0 +1,9 @@
+resource "konnect_auth_server_scopes" "my_authserverscopes" {
+  provider            = konnect-beta
+  auth_server_id      = "d32d905a-ed33-46a3-a093-d8f536af9a8a"
+  default             = true
+  description         = "...my_description..."
+  enabled             = false
+  include_in_metadata = false
+  name                = "...my_name..."
+}
