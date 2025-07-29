5e821a57872245f01ad4d343b0a551a69f41629f
Michael Heap <m@michaelheap.com>
2025-07-29 10:35:58 +0100

    Add tests for Dashboards

---
+++ tests/resources/dashboard_test.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+)
+
+func TestDashboard(t *testing.T) {
+	t.Run("smoke-test", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					ProtoV6ProviderFactories: providerFactory,
+					ConfigDirectory:          config.TestNameDirectory(),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_dashboard.my_dashboard", "name", "Example Dashboard"),
+					),
+				},
+				{
+					ProtoV6ProviderFactories: providerFactory,
+					ConfigDirectory:          config.TestNameDirectory(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectEmptyPlan(),
+						},
+					},
+				},
+			},
+		})
+	})
+}
