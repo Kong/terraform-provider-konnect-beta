b6d5670d815a4d9c29e8a3cf6997e8292c94310f
Ilya Lobkov <ilya.lobkov@konghq.com>
2025-10-09 13:41:37 +0200

    add new unit test
    Signed-off-by: Ilya Lobkov <ilya.lobkov@konghq.com>

--- tests/resources/mesh_test.go
+++ tests/resources/mesh_test.go
@@ -75,6 +75,106 @@ func TestMesh(t *testing.T) {
 
 		resource.ParallelTest(t, tfbuilder.CreatePolicyAndModifyFieldsOnIt(providerFactory, builder, mtp))
 	})
+	t.Run("create a mesh and modify fields on it", func(t *testing.T) {
+		builder := tfbuilder.NewBuilder(tfbuilder.Konnect, serverScheme, serverHost, serverPort).WithProviderProperty(tfbuilder.KonnectBeta)
+		cp := tfbuilder.NewControlPlane("e2e-test", "e2e-test", "e2e test cp")
+		mesh := tfbuilder.NewMeshBuilder("m1", "m1").
+			WithCPID(builder.ResourceAddress("mesh_control_plane", cp.ResourceName) + ".id").
+			WithDependsOn(builder.ResourceAddress("mesh_control_plane", cp.ResourceName)).
+			WithSpec(`skip_creating_initial_policies = [ "*" ]`)
+
+		builder.AddControlPlane(cp)
+
+		resource.ParallelTest(t, tfbuilder.CreateMeshAndModifyFieldsOnIt(providerFactory, builder, mesh))
+	})
+
+	t.Run("create a policy and modify fields on it", func(t *testing.T) {
+		builder := tfbuilder.NewBuilder(tfbuilder.Konnect, serverScheme, serverHost, serverPort).WithProviderProperty(tfbuilder.KonnectBeta)
+		cp := tfbuilder.NewControlPlane("e2e-test", "e2e-test", "e2e test cp")
+		builder.AddControlPlane(cp)
+		mesh := tfbuilder.NewMeshBuilder("default", "terraform-provider-kong-mesh").
+			WithCPID(builder.ResourceAddress("mesh_control_plane", cp.ResourceName) + ".id").
+			WithDependsOn(builder.ResourceAddress("mesh_control_plane", cp.ResourceName)).
+			WithSpec(`skip_creating_initial_policies = [ "*" ]`)
+		mtp := tfbuilder.NewPolicyBuilder("mesh_traffic_permission", "allow_all", "allow-all", "MeshTrafficPermission").
+			WithCPID(builder.ResourceAddress("mesh_control_plane", cp.ResourceName) + ".id").
+			WithMeshRef(builder.ResourceAddress("mesh", mesh.ResourceName) + ".name").
+			WithDependsOn(builder.ResourceAddress("mesh", mesh.ResourceName))
+		builder.AddMesh(mesh)
+
+		resource.ParallelTest(t, tfbuilder.CreatePolicyAndModifyFieldsOnIt(providerFactory, builder, mtp))
+	})
+
+	t.Run("create a policy and remove arrays on it", func(t *testing.T) {
+		builder := tfbuilder.NewBuilder(tfbuilder.Konnect, serverScheme, serverHost, serverPort).WithProviderProperty(tfbuilder.KonnectBeta)
+		cp := tfbuilder.NewControlPlane("e2e-test", "e2e-test", "e2e test cp")
+		builder.AddControlPlane(cp)
+		mesh := tfbuilder.NewMeshBuilder("default", "terraform-provider-kong-mesh").
+			WithCPID(builder.ResourceAddress("mesh_control_plane", cp.ResourceName) + ".id").
+			WithDependsOn(builder.ResourceAddress("mesh_control_plane", cp.ResourceName)).
+			WithSpec(`skip_creating_initial_policies = [ "*" ]`)
+		mtp := tfbuilder.NewPolicyBuilder("mesh_traffic_permission", "allow_all", "allow-all", "MeshTrafficPermission").
+			WithCPID(builder.ResourceAddress("mesh_control_plane", cp.ResourceName) + ".id").
+			WithMeshRef(builder.ResourceAddress("mesh", mesh.ResourceName) + ".name").
+			WithDependsOn(builder.ResourceAddress("mesh", mesh.ResourceName))
+		builder.AddMesh(mesh)
+
+		resource.ParallelTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config: builder.AddPolicy(mtp.WithSpec(`
+spec = {
+  rules = [
+    {
+      default = {
+        allow = [
+          {
+            spiffe_id = {
+              type  = "Exact"
+              value = "spiffe://hello/world"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mtp.ResourceType, mtp.ResourceName), plancheck.ResourceActionCreate),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+				{
+					Config: builder.AddPolicy(mtp.WithSpec(`
+spec = {
+  rules = [
+    {
+      default = {
+        deny = [
+          {
+            spiffe_id = {
+              type  = "Exact"
+              value = "spiffe://hello/world"
+            }
+          }
+		]
+      }
+    }
+  ]
+}`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction(builder.ResourceAddress(mtp.ResourceType, mtp.ResourceName), plancheck.ResourceActionUpdate),
+						},
+					},
+				},
+				tfbuilder.CheckReapplyPlanEmpty(builder),
+			},
+		})
+	})
 
 	t.Run("not imported resource should error out with meaningful message", func(t *testing.T) {
 		meshName := "m3"
