c390989b15ad4997c0a700e3f0bfa4626a1384de
Harsha P Dixit <harsha.dixit@konghq.com>
2025-12-03 12:02:12 +0530

    chore: bump speakeasy to v1.665.0 (#162)

--- internal/sdk/api.go
+++ internal/sdk/api.go
@@ -695,7 +695,7 @@ func (s *API) UpdateAPI(ctx context.Context, request operations.UpdateAPIRequest
 		req.Header.Set("Content-Type", reqContentType)
 	}
 
-	if err := utils.PopulateQueryParams(ctx, req, request, nil); err != nil {
+	if err := utils.PopulateQueryParams(ctx, req, request, nil, nil); err != nil {
 		return nil, fmt.Errorf("error populating query params: %w", err)
 	}
 
