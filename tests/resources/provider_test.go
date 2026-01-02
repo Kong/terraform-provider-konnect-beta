36ae15b1d09e3779879ed86003098b3dfc8ee93d
Harsha Dixit <harsha.dixit@konghq.com>
2026-01-02 12:40:26 +0530

    tests: pass id and client_secret in  input for auth server clients

--- tests/resources/provider_test.go
+++ tests/resources/provider_test.go
@@ -15,6 +15,11 @@ var (
 		"konnect":      providerserver.NewProtocol6WithError(provider.New("")()),
 		"konnect-beta": providerserver.NewProtocol6WithError(provider.New("")()),
 	}
+
+	providerFactoryWithRandom = map[string]func() (tfprotov6.ProviderServer, error){
+		"konnect":      providerserver.NewProtocol6WithError(provider.New("")()),
+		"konnect-beta": providerserver.NewProtocol6WithError(provider.New("")()),
+	}
 )
 
 func providerConfigFromEnv() (string, int, string) {
