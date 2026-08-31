3e48d3090f11b42d7ce783b752a597edad0b10bb
Harsha Dixit <harsha.dixit@konghq.com>
2026-08-31 15:37:41 +0530

    chore: release v0.22.0

--- internal/sdk/konnectbeta.go
+++ internal/sdk/konnectbeta.go
@@ -298,9 +298,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *KonnectBeta {
 	sdk := &KonnectBeta{
-		SDKVersion: "0.21.0",
+		SDKVersion: "0.22.0",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/terraform 0.21.0 2.932.9 2.0.0 github.com/kong/terraform-provider-konnect-beta/internal/sdk",
+			UserAgent:  "speakeasy-sdk/terraform 0.22.0 2.932.9 2.0.0 github.com/kong/terraform-provider-konnect-beta/internal/sdk",
 			ServerList: ServerList,
 		},
 		hooks: hooks.New(),
